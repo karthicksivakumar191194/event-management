262ff434496da1bea1f05858f64c90db592681f3
Karthick <karthik@zaigoinfotech.com>
2020-03-08 17:13:26 +0530

    Backend Design Layout - Dashboard Inital Commit

---
+++ README_PRJ.md
@@ -0,0 +1,3 @@
+./index.js
+./App.js
+./layouts/BackEnd/Admin.jsx
--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import React from 'react';
 //import './App.css';
 import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
 import AdminLayout from "./layouts/BackEnd/Admin";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/animate.min.css";
+import "./assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0";
+import "./assets/css/demo.css";
+import "./assets/css/pe-icon-7-stroke.css";
 
 export default function App() {
 
